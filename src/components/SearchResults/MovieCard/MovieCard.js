3a078b73d0e6401b4efedc639b0209a6b4514764
Andrii Levytskyi <andrii.levytskyi@everymatrix.com>
2023-11-12 03:13:40 +0200

    minor style changes

--- src/components/SearchResults/MovieCard/MovieCard.js
+++ src/components/SearchResults/MovieCard/MovieCard.js
@@ -4,9 +4,10 @@ import './MovieCard.css'; // Import the CSS file for styling
 
 const MovieCard = ({ movieData }) => {
   const { Poster, Title, Year, imdbID, Type } = movieData;
+  const cardTypeClass = Type.toLowerCase();
 
   return (
-    <div className="movie-card">
+    <div className={`movie-card ${cardTypeClass}`}>
       <img src={Poster} alt={`${Title} Poster`} className="poster" />
       <div className="info">
         <h3>{Title}</h3>
